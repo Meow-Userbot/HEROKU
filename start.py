71a36327e7480cfdbb6c9a3a91113877ed18692d
mrcoolmj <90083311+mrcoolmj@users.noreply.github.com>
2021-10-30 08:37:59 +0530

    Ok

--- start.py
+++ start.py
@@ -1,4 +1,4 @@
 from os import system # Aaja Bhai
 system ("git clone https://GitHub.com/Meow-Userbot/meow_ub && cd meow_ub && python3 -m userbot")
 # try af
-print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @UltraXchaT")
+print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @COOOLMj")
